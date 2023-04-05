34f9c1d77f797e613a43100baa3e2bae00e7ac29
AlexxMotta <alexmota@alu.ufc.br>
2023-04-05 16:31:22 -0300

    refatorando metodo create e criando metodo patch

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,21 +1,27 @@
-import { Controller, Get, HttpCode, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Res, Param, Post, Patch } from '@nestjs/common';
 
 @Controller('courses')
-export class CoursesController {
+export default class CoursesController {
   @Get()
-  findAll() {
-    return 'Listagem de cursos';
+  findAll(@Res() response) {
+    return response.status(200).send('Lista de Users');
   }
   @Post()
-  @HttpCode(204)
-  create() {
+  create(@Body() body) {
     //qualquer coisa
-    return 'user criado';
+    console.log(body);
+    return body;
   }
   //recebe apenas o id
   @Get(':id')
   findOne(@Param('id') id: string) {
     console.log(id);
     return `O id é: ${id}`;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body) {
+    console.log(id);
+    return `Atualizando o curso de id: ${id}`;
   }
 }
