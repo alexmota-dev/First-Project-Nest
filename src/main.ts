8636e39972c2652c0ffde557eb44ce0ee00f4a63
AlexxMotta <alexmota@alu.ufc.br>
2023-04-08 12:10:44 -0300

    add validações ao useGlobalPipes

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true, //não aceita requisições com informações não esperadas
+      transform: true, //tipagem com dto
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
