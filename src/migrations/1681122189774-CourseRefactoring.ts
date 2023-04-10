9396ec14084decc08a26132e1679fd577e597dec
AlexxMotta <alexmota@alu.ufc.br>
2023-04-10 08:16:40 -0300

    criando a primeira migration

--- src/migrations/1681122189774-CourseRefactoring.ts
+++ src/migrations/1681122189774-CourseRefactoring.ts
@@ -1,7 +1,15 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class CourseRefactoring1681122189774 implements MigrationInterface {
-  public async up(queryRunner: QueryRunner): Promise<void> {}
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "courses" RENAME COLUMN "name" TO "course"`,
+    );
+  }
 
-  public async down(queryRunner: QueryRunner): Promise<void> {}
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "courses" RENAME COLUMN "course" TO "name"`,
+    );
+  }
 }
